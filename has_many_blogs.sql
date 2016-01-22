1c9f55b4f109f5cb369fdfd001fd45e7b91970c3
Chaz <cmtlum73@gmail.com>
2016-01-21 19:35:06 -1000

    creates user and database, as well as user, posts, and comments tables

---
+++ has_many_blogs.sql
@@ -0,0 +1,50 @@
+DROP USER IF EXISTS has_many_user;
+
+CREATE USER has_many_user;
+
+DROP DATABASE IF EXISTS has_many_blogs;
+
+CREATE DATABASE has_many_blogs;
+
+\c has_many_blogs;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+  "id" serial NOT NULL,
+  PRIMARY KEY ( "id" ),
+  "username" varchar( 90 ) NOT NULL,
+  "first_name" varchar( 90 ) DEFAULT NULL,
+  "last_name" varchar( 90 ) DEFAULT NULL,
+  "created_at" timestamp with time zone NOT NULL DEFAULT now(),
+  "updated_at" timestamp with time zone NOT NULL DEFAULT now()
+);
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts
+(
+  "id" serial NOT NULL,
+  PRIMARY KEY ( "id" ),
+  "title" varchar( 180 ) DEFAULT NULL,
+  "url" varchar( 510 ) DEFAULT NULL,
+  "content" text DEFAULT NULL,
+  "created_at" timestamp with time zone NOT NULL DEFAULT now(),
+  "updated_at" timestamp with time zone NOT NULL DEFAULT now()
+);
+
+DROP TABLE IF EXISTS comments;
+
+CREATE TABLE comments
+(
+  "id" serial NOT NULL,
+  PRIMARY KEY ( "id" ),
+  "body" varchar( 510 ) DEFAULT NULL,
+  "created_at" timestamp with time zone NOT NULL DEFAULT now(),
+  "updated_at" timestamp with time zone NOT NULL DEFAULT now()
+);
+
+SELECT * FROM users;
+SELECT * FROM posts;
+SELECT * FROM comments;
