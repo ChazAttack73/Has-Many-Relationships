7a98a435892c2529ef60cdb83ee92a809ee6c01d
Chaz <cmtlum73@gmail.com>
2016-01-23 21:16:40 -1000

    beginning to work through queries, 1-4 complete, need remember to use aliases

--- joins.sql
+++ joins.sql
@@ -1,1 +1,24 @@
-SELECT * FROM users;
+-- \i has_many_blogs.sql;
+
+-- SELECT * FROM users;
+
+
+-- SELECT *
+-- FROM posts
+-- INNER JOIN users
+-- ON posts.id=users.id
+-- WHERE user_id=100;
+
+
+-- SELECT posts.*, first_name, last_name
+-- FROM posts
+-- INNER JOIN users
+-- ON posts.id=users.id
+-- WHERE user_id=200;
+
+
+SELECT posts.*, users.username
+FROM posts
+INNER JOIN users
+ON posts.id=users.id
+WHERE first_name='Norene' AND last_name='Schmitt';
